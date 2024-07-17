127f41df1a3efd10abf3bc048e89f74706a72431
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2024-07-17 16:30:12 +0200

    added links in nav

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <header>
+      <h1>Beta Blog</h1>
       <nav>
-        <h1>Beta Blog</h1>
+        <Link to="/">Accueil</Link>
+        <Link to="/articles">Articles</Link>
       </nav>
     </header>
   );
