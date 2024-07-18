6518740c3fca63be99f9c3829bd444f2e1cfa29c
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2024-07-18 12:23:42 +0200

    style form

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,17 @@
+import Form from "../components/form/Form";
+
 function Home() {
   return (
-    <section>
-      <h2>Bienvenue, cher•ère lecteur•trice</h2>
-      <p>
-        Sur ce blog je fais part de mes dernière découvertes sur les thèmes du
-        web et du développement en général.
-      </p>
-    </section>
+    <>
+      <section>
+        <h2>Bienvenue, cher•ère lecteur•trice</h2>
+        <p>
+          Sur ce blog je fais part de mes dernières découvertes sur les thèmes
+          du web et du développement en général.
+        </p>
+        <Form />
+      </section>
+    </>
   );
 }
 
