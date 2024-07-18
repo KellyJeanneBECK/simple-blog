020690d116ae9184a52da952ab6c12ae137fd5f7
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2024-07-18 14:23:25 +0200

    style home page

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -2,10 +2,10 @@ import styles from "./Form.module.css";
 
 function Form() {
   return (
-    <article className={styles.sectionForm}>
-      <h3 className={styles.titleForm}>
+    <section className={styles.sectionForm}>
+      <h2 className={styles.titleForm}>
         C&apos;est ici que j&apos;écris mes articles
-      </h3>
+      </h2>
       <form>
         <label htmlFor="title">Titre de l&apos;article :</label>
         <input type="text" id="title" name="title" required />
@@ -23,7 +23,7 @@ function Form() {
           Publier
         </button>
       </form>
-    </article>
+    </section>
   );
 }
 
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,6 @@
-import Form from "../components/form/Form";
+import Form from "../../components/form/Form";
+import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
 
 function Home() {
   return (
@@ -9,8 +11,13 @@ function Home() {
           Sur ce blog je fais part de mes dernières découvertes sur les thèmes
           du web et du développement en général.
         </p>
-        <Form />
       </section>
+      <Form />
+      <div className={styles.linkSection}>
+        <Link to="/articles" className={styles.homeLink}>
+          Tous les articles
+        </Link>
+      </div>
     </>
   );
 }
